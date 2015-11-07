d5424156ae80e6a9bac98485978034a1456f83e9
Keiichi Lindley <digizn@gmail.com>
2015-11-07 01:43:13 -0700

    Seeder

--- server/api/group/group.model.js
+++ server/api/group/group.model.js
@@ -4,9 +4,23 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GroupSchema = new Schema({
+  groupID: String,
+  groupTypeID: String,
+  groupTypeName: String,
+  campusID: String,
+  campusName: String,
+  creatorID: String,
+  creatorName: String,
+  leaderID: String,
+  leaderFirstName: String,
+  leaderLastName: String,
   name: String,
-  info: String,
+  description: String,
+  image: String,
+  areaID: String,
+  areaName: String,
   active: Boolean
 });
+
 
 module.exports = mongoose.model('Group', GroupSchema);
