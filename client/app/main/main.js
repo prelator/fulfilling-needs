fc6024c6ab56186a397af603187d0d79a1f85fcf
pmckay <patrick.mckay@charter.com>
2015-11-07 00:34:25 -0700

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fulfillingNeedsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
