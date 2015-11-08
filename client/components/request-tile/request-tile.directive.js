4c49fbedd2316450d6bb72a603fb4b0391c8daa9
ho0 <howard.pflugh@gmail.com>
2015-11-08 14:13:04 -0700

    wired group and request to services

--- client/components/request-tile/request-tile.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('fulfillingNeedsApp')
-  .directive('requestTile', function() {
-    return {
-      scope {
-        request: '='
-      }
-      templateUrl: 'request-tile.html'
-    };
-  });
