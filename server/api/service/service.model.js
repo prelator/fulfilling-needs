b62a81922f8217843bf10eea957c49d6a9a19125
Keiichi Lindley <digizn@gmail.com>
2015-11-08 11:35:19 -0700

    Implement creating new service route

--- server/api/service/service.model.js
+++ server/api/service/service.model.js
@@ -1,55 +1,18 @@
 'use strict';
 
-exports.all = function(){
-  return [{
-    'icon': 'avatar2.jpg',
-    'name': 'Mary Server',
-    'group': 'Group Name',
-    'needDate': '10/22/2015',
-    'description': 'I need help mowing my lawn. Need a young whipper snapper who knows how to handle weeds.',
-    'area': 'lawncare',
-    'abillity': 'some abillity',
-    'dateDelta': 'Yesterday',
-    'distanceDelta': '1.5 miles'
-  },{
-    'icon': 'avatar2.jpg',
-    'name': 'Mary Server',
-    'group': 'Group Name',
-    'needDate': '10/22/2015',
-    'description': 'I need help mowing my lawn. Need a young whipper snapper who knows how to handle weeds.',
-    'area': 'lawncare',
-    'abillity': 'some abillity',
-    'dateDelta': 'Yesterday',
-    'distanceDelta': '1.5 miles'
-  },{
-    'icon': 'avatar2.jpg',
-    'name': 'Mary Server',
-    'group': 'Group Name',
-    'needDate': '10/22/2015',
-    'description': 'I need help mowing my lawn. Need a young whipper snapper who knows how to handle weeds.',
-    'area': 'lawncare',
-    'abillity': 'some abillity',
-    'dateDelta': 'Yesterday',
-    'distanceDelta': '1.5 miles'
-  },{
-    'icon': 'avatar2.jpg',
-    'name': 'Mary Server',
-    'group': 'Group Name',
-    'needDate': '10/22/2015',
-    'description': 'I need help mowing my lawn. Need a young whipper snapper who knows how to handle weeds.',
-    'area': 'lawncare',
-    'abillity': 'some abillity',
-    'dateDelta': 'Yesterday',
-    'distanceDelta': '1.5 miles'
-  },{
-    'icon': 'avatar2.jpg',
-    'name': 'Mary Server',
-    'group': 'Group Name',
-    'needDate': '10/22/2015',
-    'description': 'I need help mowing my lawn. Need a young whipper snapper who knows how to handle weeds.',
-    'area': 'lawncare',
-    'abillity': 'some abillity',
-    'dateDelta': 'Yesterday',
-    'distanceDelta': '1.5 miles'
-  }]
-}
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ServiceSchema = new Schema({
+  icon: String,
+  name: String,
+  group: String,
+  needDate: String,
+  description: String,
+  area: String,
+  abillity: String,
+  dateDelta: String,
+  distanceDelta: String,
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
